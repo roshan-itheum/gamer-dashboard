a56da2f072f09afb00ae217c1d05f50831d3709c
roshan-itheum <105621525+roshan-itheum@users.noreply.github.com>
2022-07-01 08:22:31 +0530

    Chakra integration

--- app/entry.client.jsx
+++ app/entry.client.jsx
@@ -1,4 +1,32 @@
-import { RemixBrowser } from "@remix-run/react";
-import { hydrate } from "react-dom";
+// entry.client.tsx
+import React, { useState } from 'react'
+import { hydrate } from 'react-dom'
+import { CacheProvider } from '@emotion/react'
+import { RemixBrowser } from '@remix-run/react'
+import { ClientStyleContext } from './store/chakraContext'
+import createEmotionCache from './utils/chakra/createEmotionCache'
 
-hydrate(<RemixBrowser />, document);
+
+
+
+
+function ClientCacheProvider({ children }) {
+  const [cache, setCache] = useState(createEmotionCache())
+
+  function reset() {
+    setCache(createEmotionCache())
+  }
+
+  return (
+    <ClientStyleContext.Provider value={{ reset }}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
+  )
+}
+
+hydrate(
+  <ClientCacheProvider>
+    <RemixBrowser />
+  </ClientCacheProvider>,
+  document,
+)
